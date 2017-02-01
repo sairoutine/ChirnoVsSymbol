eabba612d2db54f791212c26e46786e36e7b542f
sairoutine <sairoutine@gmail.com>
2017-02-01 23:12:33 +0900

    add: mock

---
+++ src/js/game.js
@@ -0,0 +1,10 @@
+'use strict';
+var core = require('./hakurei').core;
+var util = require('./hakurei').util;
+
+var Game = function(canvas) {
+
+};
+util.inherit(Game, core);
+
+module.exports = Game;
