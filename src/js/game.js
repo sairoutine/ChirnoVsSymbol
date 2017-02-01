f6337418833ee505dd4eaeeea97eefe6c8ca0ed6
sairoutine <sairoutine@gmail.com>
2017-02-01 23:37:45 +0900

    update

--- src/js/game.js
+++ src/js/game.js
@@ -3,8 +3,14 @@ var core = require('./hakurei').core;
 var util = require('./hakurei').util;
 
 var Game = function(canvas) {
-
+	core.apply(this, arguments);
 };
 util.inherit(Game, core);
+
+Game.prototype.run = function () {
+	core.prototype.run.apply(this, arguments);
+
+	console.log("run: " + this.frame_count);
+};
 
 module.exports = Game;
