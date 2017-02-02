d9f278615e1e1a0a80a5e8007948fe550ee1de0b
sairoutine <sairoutine@gmail.com>
2017-02-02 22:51:59 +0900

    add: enemy_appear class

---
+++ src/js/logic/enemy_appear.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var EnemyAppear = function() {
+};
+
+EnemyAppear.prototype.init = function() {
+};
+EnemyAppear.prototype.update = function() {
+};
+EnemyAppear.prototype.is_occur = function() {
+};
+EnemyAppear.prototype.getAppearEnemies = function() {
+};
+module.exports = EnemyAppear;
--- src/js/scene/stg.js
+++ src/js/scene/stg.js
@@ -2,22 +2,47 @@
 
 var base_scene = require('../hakurei').scene.base;
 var util = require('../hakurei').util;
+var PoolManager = require('../hakurei').object.pool_manager;
 
 var Chara = require('../object/chara');
+var EnemyAppear = require('../logic/enemy_appear');
 
 var SceneStg = function(core) {
 	base_scene.apply(this, arguments);
 
 	this.chara = new Chara(this);
 	this.addObject(this.chara);
+
+	this.enemies = new PoolManager(this);
+	this.addObject(this.enemies);
+
+	this.enemy_appear = new EnemyAppear();
 };
 util.inherit(SceneStg, base_scene);
 
+SceneStg.prototype.init = function(){
+	base_scene.prototype.init.apply(this, arguments);
+	this.enemy_appear.init();
+};
+
+SceneStg.prototype.beforeDraw = function(){
+	base_scene.prototype.beforeDraw.apply(this, arguments);
+
+	// appear enemy
+	this.enemy_appear.update();
+	if (this.enemy_appear.is_occur()) {
+		this.enemies.addObjects(this.enemy_appear.getAppearEnemies());
+	}
+};
+
 SceneStg.prototype.draw = function(){
 	var ctx = this.core.ctx;
+
+	// draw background color
 	ctx.fillStyle = util.hexToRGBString("E2FFFC");
 	ctx.fillRect(0, 0, this.core.width, this.core.height);
 
+	// draw objects
 	base_scene.prototype.draw.apply(this, arguments);
 };
 
