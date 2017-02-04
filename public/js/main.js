33f2333cc6770666645782806f588965d5b4ad43
sairoutine <sairoutine@gmail.com>
2017-02-04 23:44:49 +0900

    compile js

--- public/js/main.js
+++ public/js/main.js
@@ -21,7 +21,7 @@ Game.prototype.init = function () {
 
 module.exports = Game;
 
-},{"./hakurei":2,"./scene/loading":15,"./scene/stg":16}],2:[function(require,module,exports){
+},{"./hakurei":2,"./scene/loading":18,"./scene/stg":19}],2:[function(require,module,exports){
 'use strict';
 
 module.exports = require("./hakurejs/index");
@@ -294,6 +294,7 @@ module.exports = {
 	},
 	object: {
 		base: require("./object/base"),
+		sprite: require("./object/sprite"),
 		pool_manager: require("./object/pool_manager"),
 	},
 	asset_loader: {
@@ -302,17 +303,24 @@ module.exports = {
 	},
 };
 
-},{"./asset_loader/image":3,"./constant":4,"./core":5,"./object/base":7,"./object/pool_manager":8,"./scene/base":9,"./util":10}],7:[function(require,module,exports){
+},{"./asset_loader/image":3,"./constant":4,"./core":5,"./object/base":7,"./object/pool_manager":8,"./object/sprite":9,"./scene/base":10,"./util":11}],7:[function(require,module,exports){
 'use strict';
+
+var util = require('../util');
+
+var id = 0;
 
 var ObjectBase = function(scene) {
 	this.scene = scene;
 	this.core = scene.core;
+	this.id = ++id;
 
 	this.frame_count = 0;
 
 	this.x = 0;
 	this.y = 0;
+
+	this.velocity = {magnitude:0, theta:0};
 };
 
 ObjectBase.prototype.init = function(){
@@ -325,6 +333,7 @@ ObjectBase.prototype.init = function(){
 ObjectBase.prototype.beforeDraw = function(){
 	this.frame_count++;
 
+	this.move();
 };
 
 ObjectBase.prototype.draw = function(){
@@ -333,10 +342,35 @@ ObjectBase.prototype.draw = function(){
 ObjectBase.prototype.afterDraw = function(){
 };
 
+ObjectBase.prototype.move = function() {
+	var x = util.calcMoveXByVelocity(this.velocity);
+	var y = util.calcMoveYByVelocity(this.velocity);
+
+	this.x += x;
+	this.y += y;
+};
+
+
+
+ObjectBase.prototype.leftX = function() {
+	return this.x;
+};
+ObjectBase.prototype.rightX = function() {
+	return this.x + this.width;
+};
+ObjectBase.prototype.upY = function() {
+	return this.y;
+};
+ObjectBase.prototype.downY = function() {
+	return this.y + this.height;
+};
+ObjectBase.prototype.setVelocity = function(velocity) {
+	this.velocity = velocity;
+};
 module.exports = ObjectBase;
 
 
-},{}],8:[function(require,module,exports){
+},{"../util":11}],8:[function(require,module,exports){
 'use strict';
 
 // TODO: add pooling logic
@@ -344,51 +378,150 @@ module.exports = ObjectBase;
 var base_object = require('./base');
 var util = require('../util');
 
-var PoolManager = function(scene) {
+var PoolManager = function(scene, Class) {
 	base_object.apply(this, arguments);
 
-	this.objects = [];
+	this.Class = Class;
+	this.objects = {};
 };
 util.inherit(PoolManager, base_object);
 
 PoolManager.prototype.init = function() {
 	base_object.prototype.init.apply(this, arguments);
 
-	this.objects = [];
+	this.objects = {};
 };
 
 PoolManager.prototype.beforeDraw = function(){
 	base_object.prototype.beforeDraw.apply(this, arguments);
 
-	for(var i = 0, len = this.objects.length; i < len; i++) {
-		this.objects[i].beforeDraw();
+	for(var id in this.objects) {
+		this.objects[id].beforeDraw();
 	}
 };
 
 PoolManager.prototype.draw = function(){
 	base_object.prototype.draw.apply(this, arguments);
-	for(var i = 0, len = this.objects.length; i < len; i++) {
-		this.objects[i].draw();
+	for(var id in this.objects) {
+		this.objects[id].draw();
 	}
 };
 
 PoolManager.prototype.afterDraw = function(){
 	base_object.prototype.afterDraw.apply(this, arguments);
-	for(var i = 0, len = this.objects.length; i < len; i++) {
-		this.objects[i].afterDraw();
+	for(var id in this.objects) {
+		this.objects[id].afterDraw();
 	}
 };
 
-PoolManager.prototype.addObject = function(object){
-	this.objects.push(object);
-};
+PoolManager.prototype.create = function() {
+	var object = new this.Class(this.scene);
+	object.init.apply(object, arguments);
 
-PoolManager.prototype.addObjects = function(objects) {
-	 this.objects = this.objects.concat(objects);
+	this.objects[object.id] = object;
+
+	return object;
 };
 module.exports = PoolManager;
 
-},{"../util":10,"./base":7}],9:[function(require,module,exports){
+},{"../util":11,"./base":7}],9:[function(require,module,exports){
+'use strict';
+var base_object = require('./base');
+var util = require('../util');
+
+var Sprite = function(scene) {
+	base_object.apply(this, arguments);
+
+	this.current_sprite_index = 0;
+};
+util.inherit(Sprite, base_object);
+
+Sprite.prototype.init = function(){
+	base_object.prototype.init.apply(this, arguments);
+
+	this.current_sprite_index = 0;
+};
+
+Sprite.prototype.beforeDraw = function(){
+	base_object.prototype.beforeDraw.apply(this, arguments);
+
+	// animation sprite
+	if(this.frame_count % this.spriteAnimationSpan() === 0) {
+		this.current_sprite_index++;
+		if(this.current_sprite_index >= this.spriteIndices().length) {
+			this.current_sprite_index = 0;
+		}
+	}
+};
+Sprite.prototype.draw = function(){
+	base_object.prototype.draw.apply(this, arguments);
+
+	var image = this.core.image_loader.getImage(this.spriteName());
+
+	var ctx = this.core.ctx;
+
+	ctx.save();
+
+	// set position
+	ctx.translate(this.x, this.y);
+
+	// rotate
+	var rotate = util.thetaToRadian(this.velocity.theta + this.rotateAdjust());
+	ctx.rotate(rotate);
+
+	var sprite_width  = this.spriteWidth();
+	var sprite_height = this.spriteHeight();
+	if(!sprite_width)  sprite_width = image.width;
+	if(!sprite_height) sprite_height = image.height;
+
+	var width  = sprite_width * this.scale();
+	var height = sprite_height * this.scale();
+
+	ctx.drawImage(image,
+		// sprite position
+		sprite_width * this.spriteIndexX(), sprite_height * this.spriteIndexY(),
+		// sprite size to get
+		sprite_width,                       sprite_height,
+		// adjust left x, up y because of x and y indicate sprite center.
+		-width/2,                           -height/2,
+		// sprite size to show
+		width,                              height
+	);
+	ctx.restore();
+};
+
+Sprite.prototype.spriteName = function(){
+	throw new Error("spriteName method must be overridden.");
+};
+Sprite.prototype.spriteIndexX = function(){
+	return this.spriteIndices()[this.current_sprite_index].x;
+};
+Sprite.prototype.spriteIndexY = function(){
+	return this.spriteIndices()[this.current_sprite_index].y;
+};
+Sprite.prototype.spriteAnimationSpan = function(){
+	return 0;
+};
+Sprite.prototype.spriteIndices = function(){
+	return [{x: 0, y: 0}];
+};
+Sprite.prototype.spriteWidth = function(){
+	return 0;
+};
+Sprite.prototype.spriteHeight = function(){
+	return 0;
+};
+Sprite.prototype.rotateAdjust = function(){
+	return 0;
+};
+Sprite.prototype.scale = function(){
+	return 1;
+};
+
+
+module.exports = Sprite;
+
+},{"../util":11,"./base":7}],10:[function(require,module,exports){
 'use strict';
 
 var SceneBase = function(core) {
@@ -435,7 +568,7 @@ SceneBase.prototype.addObject = function(object){
 module.exports = SceneBase;
 
 
-},{}],10:[function(require,module,exports){
+},{}],11:[function(require,module,exports){
 'use strict';
 var Util = {
 	inherit: function( child, parent ) {
@@ -455,6 +588,12 @@ var Util = {
 	thetaToRadian: function(theta) {
 		return theta * Math.PI / 180;
 	},
+	calcMoveXByVelocity: function(velocity) {
+		return velocity.magnitude * Math.cos(Util.thetaToRadian(velocity.theta));
+	},
+	calcMoveYByVelocity: function(velocity) {
+		return velocity.magnitude * Math.sin(Util.thetaToRadian(velocity.theta));
+	},
 	hexToRGBString: function(h) {
 		var hex16 = (h.charAt(0) === "#") ? h.substring(1, 7) : h;
 		var r = parseInt(hex16.substring(0, 2), 16);
@@ -467,13 +606,20 @@ var Util = {
 
 module.exports = Util;
 
-},{}],11:[function(require,module,exports){
+},{}],12:[function(require,module,exports){
 'use strict';
 
-var EnemyAppear = function() {
+var EnemyAppear = function(enemy_pool_manager) {
+	this.enemies = enemy_pool_manager;
 };
 
 EnemyAppear.prototype.init = function() {
+};
+EnemyAppear.prototype.exec = function() {
+	this.update();
+	if (this.is_occur()) {
+		this.enemies.addObjects(this.getAppearEnemies());
+	}
 };
 EnemyAppear.prototype.update = function() {
 };
@@ -483,7 +629,7 @@ EnemyAppear.prototype.getAppearEnemies = function() {
 };
 module.exports = EnemyAppear;
 
-},{}],12:[function(require,module,exports){
+},{}],13:[function(require,module,exports){
 'use strict';
 var Game = require('./game');
 
@@ -548,22 +694,25 @@ window.changeFullScreen = function () {
 };
 */
 
-},{"./game":1}],13:[function(require,module,exports){
+},{"./game":1}],14:[function(require,module,exports){
 'use strict';
-var base_object = require('../hakurei').object.base;
+var sprite = require('../hakurei').object.sprite;
 var util = require('../hakurei').util;
 
 var CONSTANT = require('../hakurei').constant;
 
+
+var SHOT_SPAN = 6;
+
 var Chara = function(scene) {
-	base_object.apply(this, arguments);
+	sprite.apply(this, arguments);
 
 	this.velocity = {theta: 0, magnitude: 10};
 };
-util.inherit(Chara, base_object);
+util.inherit(Chara, sprite);
 
 Chara.prototype.init = function(){
-	base_object.prototype.init.apply(this, arguments);
+	sprite.prototype.init.apply(this, arguments);
 
 	this.x = this.core.width/2;
 	this.y = this.core.height/2;
@@ -581,7 +730,9 @@ var BUTTON_RIGHT_MAX_THETA = 0;
 
 
 Chara.prototype.beforeDraw = function(){
-	base_object.prototype.beforeDraw.apply(this, arguments);
+	sprite.prototype.beforeDraw.apply(this, arguments);
+
+	// decide theta to move
 	if(this.core.isKeyDown(CONSTANT.BUTTON_LEFT) &&
 			this.core.isKeyDown(CONSTANT.BUTTON_DOWN)) {
 		this.velocity.theta=BUTTON_DOWN_LEFT_MAX_THETA;
@@ -610,71 +761,108 @@ Chara.prototype.beforeDraw = function(){
 	else if(this.core.isKeyDown(CONSTANT.BUTTON_RIGHT)) {
 		this.velocity.theta=BUTTON_RIGHT_MAX_THETA;
 	}
+
+	// shot automatically
+	if (this.frame_count % SHOT_SPAN === 0) {
+		var magnitude = 12;
+		var theta = this.velocity.theta;
+		this.scene.shots.create(this.x, this.y, theta);
+		this.scene.shots.create(this.x, this.y, theta).moveByVelocity({magnitude: magnitude, theta: theta + 90});
+		this.scene.shots.create(this.x, this.y, theta).moveByVelocity({magnitude: magnitude, theta: theta - 90});
+	}
+};
+Chara.prototype.move = function() {
+	// chara doesn't move because stage does move
 };
 
 Chara.prototype.calcMoveX = function() {
-	var move_x = this.velocity.magnitude * Math.cos(util.thetaToRadian(this.velocity.theta));
-	return move_x;
+	return util.calcMoveXByVelocity(this.velocity);
 };
 
 Chara.prototype.calcMoveY = function() {
-	var move_y = this.velocity.magnitude * Math.sin(util.thetaToRadian(this.velocity.theta));
-	return move_y;
+	return util.calcMoveYByVelocity(this.velocity);
 };
 
-Chara.prototype.draw = function(){
-	base_object.prototype.draw.apply(this, arguments);
-
-	var image = this.core.image_loader.getImage("chara");
-
-	var ctx = this.core.ctx;
-
-	ctx.save();
-
-	// set position
-	ctx.translate(this.x, this.y);
-
-	// rotate
-	var rotate = util.thetaToRadian(this.velocity.theta);
-	ctx.rotate(rotate);
-
-	var width  = this.spriteWidth()  * this.scale();
-	var height = this.spriteHeight() * this.scale();
-
-	ctx.drawImage(image,
-		// スプライトの取得位置
-		this.spriteWidth()  * this.spriteIndexX(), this.spriteHeight() * this.spriteIndexY(),
-		// スプライトのサイズ
-		this.spriteWidth(),                   this.spriteHeight(),
-		// x, yがオブジェクトの真ん中を指定しているので、左上をx, yの始点に変更
-		-width/2,                             -height/2,
-		// オブジェクトのゲーム上のサイズ
-		width,                                height
-	);
-	ctx.restore();
+Chara.prototype.spriteName = function(){
+	return "chara";
 };
 
-Chara.prototype.spriteIndexX = function(){
-	return 3;
+Chara.prototype.spriteIndices = function(){
+	return [{x: 4, y: 0}, {x: 5, y: 0}];
 };
-Chara.prototype.spriteIndexY = function(){
-	return 0;
+Chara.prototype.spriteAnimationSpan = function(){
+	return 6;
 };
-
 Chara.prototype.spriteWidth = function(){
 	return 80;
 };
 Chara.prototype.spriteHeight = function(){
 	return 96;
 };
-Chara.prototype.scale = function(){
-	return 1;
-};
-
 
 module.exports = Chara;
 
-},{"../hakurei":2}],14:[function(require,module,exports){
+},{"../hakurei":2}],15:[function(require,module,exports){
+'use strict';
+var base_object = require('../hakurei').object.base;
+var util = require('../hakurei').util;
+
+var Enemy = function(scene) {
+	base_object.apply(this, arguments);
+};
+util.inherit(Enemy, base_object);
+
+module.exports = Enemy;
+
+},{"../hakurei":2}],16:[function(require,module,exports){
+'use strict';
+var sprite = require('../hakurei').object.sprite;
+var util = require('../hakurei').util;
+
+var MAGNITUDE = 10;
+
+var Shot = function(scene) {
+	sprite.apply(this, arguments);
+};
+util.inherit(Shot, sprite);
+
+Shot.prototype.init = function(x, y, theta) {
+	sprite.prototype.init.apply(this, arguments);
+
+	this.x = x;
+	this.y = y;
+	this.setVelocity({magnitude: MAGNITUDE, theta: theta});
+};
+
+Shot.prototype.moveByVelocity = function(velocity){
+	var x = util.calcMoveXByVelocity(velocity);
+	var y = util.calcMoveYByVelocity(velocity);
+
+	this.x += x;
+	this.y += y;
+};
+Shot.prototype.spriteName = function(){
+	return "shot";
+};
+Shot.prototype.spriteIndices = function(){
+	return [{x: 5, y: 13}];
+};
+Shot.prototype.spriteWidth = function(){
+	return 18;
+};
+Shot.prototype.spriteHeight = function(){
+	return 16;
+};
+Shot.prototype.rotateAdjust = function(){
+	return 90;
+};
+
+
+
+
+module.exports = Shot;
+
+},{"../hakurei":2}],17:[function(require,module,exports){
 'use strict';
 var base_object = require('../hakurei').object.base;
 var util = require('../hakurei').util;
@@ -685,19 +873,6 @@ var Stage = function(scene) {
 	this.height = 2000;
 };
 util.inherit(Stage, base_object);
-
-Stage.prototype.leftX = function() {
-	return this.x;
-};
-Stage.prototype.rightX = function() {
-	return this.x + this.width;
-};
-Stage.prototype.upY = function() {
-	return this.y;
-};
-Stage.prototype.downY = function() {
-	return this.y + this.height;
-};
 
 Stage.prototype.draw = function(){
 	base_object.prototype.draw.apply(this, arguments);
@@ -717,7 +892,7 @@ Stage.prototype.draw = function(){
 
 module.exports = Stage;
 
-},{"../hakurei":2}],15:[function(require,module,exports){
+},{"../hakurei":2}],18:[function(require,module,exports){
 'use strict';
 
 var base_scene = require('../hakurei').scene.base;
@@ -731,6 +906,7 @@ util.inherit(SceneLoading, base_scene);
 SceneLoading.prototype.init = function() {
 	base_scene.prototype.init.apply(this, arguments);
 	this.core.image_loader.loadImage("chara", "./image/chirno.png");
+	this.core.image_loader.loadImage("shot", "./image/shot.png");
 };
 
 SceneLoading.prototype.beforeDraw = function() {
@@ -754,7 +930,7 @@ SceneLoading.prototype.draw = function(){
 
 module.exports = SceneLoading;
 
-},{"../hakurei":2}],16:[function(require,module,exports){
+},{"../hakurei":2}],19:[function(require,module,exports){
 'use strict';
 
 var base_scene = require('../hakurei').scene.base;
@@ -764,22 +940,26 @@ var CONSTANT = require('../hakurei').constant;
 
 var Chara = require('../object/chara');
 var Stage = require('../object/stage');
+var Enemy = require('../object/enemy');
+var Shot  = require('../object/shot');
 var EnemyAppear = require('../logic/enemy_appear');
 
 var SceneStg = function(core) {
 	base_scene.apply(this, arguments);
 
+	this.stage = new Stage(this);
+	this.addObject(this.stage);
+
+	this.shots = new PoolManager(this, Shot);
+	this.addObject(this.shots);
+
 	this.chara = new Chara(this);
 	this.addObject(this.chara);
 
-	this.enemies = new PoolManager(this);
+	this.enemies = new PoolManager(this, Enemy);
 	this.addObject(this.enemies);
 
-	this.enemy_appear = new EnemyAppear();
-
-
-	this.stage = new Stage(this);
-	this.addObject(this.stage);
+	this.enemy_appear = new EnemyAppear(this.enemies);
 };
 util.inherit(SceneStg, base_scene);
 
@@ -792,10 +972,7 @@ SceneStg.prototype.beforeDraw = function(){
 	base_scene.prototype.beforeDraw.apply(this, arguments);
 
 	// appear enemy
-	this.enemy_appear.update();
-	if (this.enemy_appear.is_occur()) {
-		this.enemies.addObjects(this.enemy_appear.getAppearEnemies());
-	}
+	this.enemy_appear.exec();
 
 	if(this.core.isKeyDown(CONSTANT.BUTTON_Z)) {
 		var x = this.chara.calcMoveX();
@@ -835,4 +1012,4 @@ SceneStg.prototype.draw = function(){
 
 module.exports = SceneStg;
 
-},{"../hakurei":2,"../logic/enemy_appear":11,"../object/chara":13,"../object/stage":14}]},{},[12]);
+},{"../hakurei":2,"../logic/enemy_appear":12,"../object/chara":14,"../object/enemy":15,"../object/shot":16,"../object/stage":17}]},{},[13]);
